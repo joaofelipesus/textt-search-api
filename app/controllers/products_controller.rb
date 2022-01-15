43477eef76615b17963c4798f6caa6ac8e7c62d3
joaofelipe <joaofelipe1294@gmail.com>
2022-01-15 17:23:13 -0300

    feat: add controller search by engine coverage.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,16 @@
 
 class ProductsController < ApplicationController
   def index
-    products = Product.all
+    products = search
     render('products/index', formats: :json, locals: { products: })
+  end
+
+  private
+
+  def search
+    search_engine = params[:engine]
+    return Product.all unless search_engine
+
+    SearchEngines::Ilike.search(params[:search_by]) if search_engine == 'ilike'
   end
 end
--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -5,6 +5,7 @@ require 'rails_helper'
 RSpec.describe 'Products', type: :request do
   describe 'GET api/products' do
     let!(:category) { create(:category, name: 'First category') }
+
     let!(:product) do
       create(
         :product,
@@ -14,6 +15,7 @@ RSpec.describe 'Products', type: :request do
         description: 'Some description'
       )
     end
+
     let(:expected_response) do
       [
         {
@@ -31,6 +33,18 @@ RSpec.describe 'Products', type: :request do
     it 'returns a products list' do
       get('/api/products')
       expect(JSON.parse(response.body)).to match expected_response
+    end
+
+    context 'when search using ilike' do
+      let(:search_engine) { double(:search_engine) }
+
+      it 'calls ilike search engine' do
+        allow(SearchEngines::Ilike).to receive(:new).with('some cool term').and_return(search_engine)
+        allow(search_engine).to receive(:search).and_return([])
+        expect(SearchEngines::Ilike).to receive(:search).at_least(:once)
+
+        get('/api/products?engine=ilike&search_by=some+cool+term')
+      end
     end
   end
 end
